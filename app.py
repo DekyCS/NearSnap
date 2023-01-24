c78e4e23c268d9ddb223554491a51cae3b7d5025
Omar Lahlou Mimi <omarlahmimi@gmail.com>
2023-01-23 22:49:25 -0500

    Location redirection, loading into index with a loction, etc

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, url_for
 from cs50 import SQL
-from flask import redirect, render_template , request, session
+from flask import redirect, render_template , request, session, jsonify, make_response
 from flask_session import Session
 from flask_sockets import Sockets
 
 
 # from werkzeug.security import generate_password_hash, check_password_hash
-
-
 
 app = Flask(__name__)
 
@@ -16,29 +14,30 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 sockets = Sockets(app)
 
-
-
 app.secret_key = "uytgfviygf9876gyfv786gyf876t7678ft"
 
 db = SQL("sqlite:///media.db")
 
+location = []
+app.config["session_location"] = 0
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
+    app.config["session_location"] += 1
+
     if session.get("userid") is None:
          return redirect("/login")
     
-    elif session.get("location") is None:
+    elif app.config["session_location"] % 3 != 0:
         return redirect("/loading")
 
     else:
         
+        app.config["session_location"] = 0
 
-        latitude = session["latitude"]
-        longitude = session["longitude"]
-        posts = db.execute(f"SELECT * FROM posts WHERE (6371 * acos(cos(radians({latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians({longitude})) + sin(radians({latitude})) * sin(radians(latitude)))) < 1  ")
-        return render_template("index.html", posts)
+        #posts = db.execute(f"SELECT * FROM posts WHERE (6371 * acos(cos(radians({latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians({longitude})) + sin(radians({latitude})) * sin(radians(latitude)))) < 1  ")
+        return render_template("index.html")
         
 
 @app.route("/login", methods=["GET", "POST"])
@@ -87,47 +86,28 @@ def register():
                 db.execute("INSERT INTO users (username, password) VALUES(?, ?)", username, password)
                 return redirect("/")
 
-@app.route("/upload")
-def upload():
-    return render_template("upload.html")
-
-# @app.route("/addpic" , methods=["POST"])
-
-
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
     session.clear()
     return redirect("/")
 
-
-@app.route("/location", methods=["POST"])
-def location():
-
-    latitude = request.json['latitude']
-    longitude = request.json['longitude']
-
-
-    session['latitude'] = latitude
-    session['longitude'] = longitude
-
-    return redirect("/")
-    
-
 @app.route("/loading")
 def loading():
-
-    
     return render_template("loading.html")
 
+@app.route("/loading/create-entry", methods=["POST"])
+def create_entry():
 
-@app.route("/reload")
-def reload():
-    session["latitude"].clear()
-    session["longitude"].clear()
-    return redirect("/loading")
+    req = request.get_json()
 
+    app.config["session_location"] += 1
 
+    location.append(req["longitude"])
+    location.append(req["latitude"])
 
+    res = make_response(jsonify({"message": "Message Received"}), 200)
+
+    return "doesn't matter"
 
             
 
