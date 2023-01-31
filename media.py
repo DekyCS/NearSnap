594d43a2b4f390088da1aafa5920b920994bca99
Omar Lahlou Mimi <omarlahmimi@gmail.com>
2023-01-30 22:53:52 -0500

    mySQL !!!!

--- media.py
+++ media.py
@@ -4,38 +4,23 @@ from cs50 import SQL
 from flask_session import Session
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
-import sqlite3
-import math
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# MySQL configurations
+app.config['MYSQL_HOST'] = '35.229.66.34'
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = 'nearfaceo&a2023'
+app.config['MYSQL_DB'] = 'nearsnap'
+
+mysql = MySQL(app)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 app.config["secret_key"] = os.urandom(100000000)
-app.config['WTF_CSRF_SECRET_KEY'] = app.config["secret_key"]
-
-
-db = SQL("sqlite:///media.db")
-
-def acos(x):
-    return math.acos(x)
-
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect('media.db')
-        g.db.create_function("acos", 1, acos)
-    return g.db
-
-@app.teardown_appcontext
-def close_db(error):
-    if 'db' in g:
-        g.db.close()
-
-
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
@@ -63,7 +48,7 @@ def convert_time_format(time_str):
 def time_since(posts):
     if posts[3] != "":
         time = posts[3]
-        time = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+        time = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
         since = datetime.datetime.now() - time
         return convert_time_format(since)
     return None
@@ -85,26 +70,31 @@ def index():
 
     else:
         
+
+        cursor = mysql.connection.cursor()
+
         app.config["session_location"] = 0
 
         # print(app.config["location"])
         latitude = app.config["location"][1]
         longitude = app.config["location"][0]
 
-        db_ = get_db()
-        cursor = db_.cursor()
-        posts = cursor.execute(f"SELECT * FROM posts WHERE (6371 * acos(cos(radians({latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians({longitude})) + sin(radians({latitude})) * sin(radians(latitude)))) <= 10 ")
+        cursor.execute(f"SELECT * FROM posts WHERE (6371 * acos(cos(radians({latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians({longitude})) + sin(radians({latitude})) * sin(radians(latitude)))) <= 10")
         posts = cursor.fetchall()
         posts_list = []
         
         for post in posts:
-            print("Printing", post[1])
+            cursor.execute("SELECT users.username, user_id FROM posts JOIN users ON posts.user_id = users.id WHERE user_id=%s LIMIT 1", (post[1],))
+            user = cursor.fetchall()
             posts_list.append({
             'post': post,
             'time_since': time_since(post),
-            'username': db.execute("SELECT users.username, user_id FROM posts JOIN users ON posts.user_id = users.id WHERE user_id=? LIMIT 1", post[1])
-                    
+            'username': user                 
         })
+
+        print(posts_list[0]["post"][6])
+
+        cursor.close()
 
         return render_template("index.html", posts_list=posts_list[::-1])
         
@@ -142,11 +132,17 @@ def posting():
             if not app.config["location"]:
                 return redirect("/login")
             else:
+
+                cursor = mysql.connection.cursor()
+
+
                 latitude = app.config["location"][1]
                 longitude = app.config["location"][0]
                 caption = request.form.get("caption")
-                time_created= datetime.datetime.now(pytz.timezone('US/Eastern'))
-                db.execute("INSERT INTO posts (user_id, created_at, content, likes, caption, latitude, longitude) VALUES(?, ?, ?, ?, ?, ?, ?)", session["userid"], time_created, filename, 0 , caption , latitude, longitude)
+                time_created= datetime.datetime.now()
+                cursor.execute("INSERT INTO posts (user_id, created_at, content, caption, latitude, longitude) VALUES(%s, %s, %s, %s, %s, %s)", (session["userid"], time_created, filename, caption, latitude, longitude))
+                mysql.connection.commit()
+                cursor.close()
 
 
         return redirect("/")
@@ -164,19 +160,26 @@ def login():
 
         username = request.form.get("username")
 
-        userid = db.execute("SELECT id FROM users WHERE username = ?", username)
+        cursor = mysql.connection.cursor()
+
+        cursor.execute("SELECT id FROM users WHERE username = %s", (username,))
+        userid = cursor.fetchall()
 
         if not userid:
             flash('Wrong Username', category='error')
             return render_template("login.html")
 
-        hash = db.execute("SELECT password FROM users where id = ?", userid[0]["id"])
+        cursor.execute("SELECT password FROM users where id = %s", (userid[0][0],))
 
-        if not check_password_hash(hash[0]["password"], request.form.get("password")):
+        hash = cursor.fetchall()
+
+        if not check_password_hash(hash[0][0], request.form.get("password")):
             flash('Wrong Password', category='error')
             return render_template("login.html" )
          
-        session["userid"] = userid[0]["id"]
+        session["userid"] = userid[0][0]
+
+        cursor.close()
           
         return redirect("/")
 
@@ -193,17 +196,25 @@ def register():
         password = request.form.get("password")
         password_confirm = request.form.get("password2")
 
-        exist = db.execute("SELECT * FROM users WHERE username = ?", username)
+        cursor = mysql.connection.cursor()
 
-        print(generate_password_hash(password))
+        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        exist = cursor.fetchall()
+
+        print(exist)
 
         if not exist:
             if password != password_confirm:
                 return redirect("/register")
             else:
-                time_created= datetime.datetime.now(pytz.timezone('US/Eastern'))
-                db.execute("INSERT INTO users (username, password, created_at) VALUES(?, ?, ?)", username, generate_password_hash(password), time_created)
+                time_created = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(time_created)
+                cursor.execute("INSERT INTO users (username, password, created_at) VALUES(%s, %s, %s)", (username, generate_password_hash(password), time_created,))
+                mysql.connection.commit()
+                cursor.close()
                 return redirect("/")
+        else:
+            return redirect("/register")
 
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
@@ -230,7 +241,8 @@ def create_entry():
 
     return "doesn't matter"
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
              
